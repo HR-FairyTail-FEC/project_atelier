c42282909a074c55e56697cb0aec04129c9f4789
garofalof <fgarofalo1493@gmail.com>
2022-04-01 15:19:48 -0700

    fixed api routes and progressed on carousel

--- public/src/Overview.jsx
+++ public/src/Overview.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Overview(props) {
-  let [featured, setFeatured] = useState(0);
-  let [active, setActive] = useState(0);
   let display;
   let feature;
+  let photos;
   let details = props.details;
+  let [featured, setFeatured] = useState(0);
+  let [active, setActive] = useState(0);
+  let [start, setStart] = useState(0);
+  let [end, setEnd] = useState(3);
 
   const handleHover = (event) => {
     setActive(event.target.id);
   }
-
   const handleSelect = (event) => {
     setFeatured(event.target.id);
+    setActive(event.target.id);
   }
 
   if (details.length < 1) {
@@ -22,9 +25,9 @@ function Overview(props) {
       <div className="product-detail-container"></div>
     </div>
   } else {
+    photos = details.styles.results[0].photos;
     let featureUrl = details.styles.results[0].photos[featured].thumbnail_url;
-    feature = <img className="feature-img" src={featureUrl}></img>
-    let photos = details.styles.results[0].photos;
+    feature = <img className="feature-img" src={featureUrl}></img>;
     let mapped = [];
     for (let i = 0; i < photos.length; i++) {
       let photo = photos[i];
@@ -39,6 +42,7 @@ function Overview(props) {
       <div className="carousel-container">
         <div className="thumbnail-container">
           {mapped}
+          <img className="thumbnail-arrow" src="https://images.squarespace-cdn.com/content/v1/5bbd01503560c334e3d24981/1551746621260-M55SZ54KVQJWN784VA38/arrow-down.png"></img>
         </div>
         <div>
           {feature}
--- public/src/ProductDetail.jsx
+++ public/src/ProductDetail.jsx
@@ -12,14 +12,13 @@ function ProductDetail() {
   useLayoutEffect(() => {
     const promises = [];
     let result = [];
-    const endpoints = [`/api/products?product_id=${id}`, `/api/products/${id}/styles`, `/api/products/${id}/related`, `/api/reviews?product_id=${id}&sort=relevant`, `/api/reviews/meta?product_id=${id}`, `/api/qa/questions?product_id=${id}`, '/api/cart'];
+    const endpoints = [`/api/products/${id}`, `/api/products/${id}/styles`, `/api/products/${id}/related`, `/api/reviews?product_id=${id}&sort=relevant`, `/api/reviews/meta?product_id=${id}`, `/api/qa/questions?product_id=${id}`, '/api/cart'];
     const fetch = async () => {
       for (let i = 0; i < endpoints.length; i += 1) {
         promises.push(axios.get(`http://localhost:3000${endpoints[i]}`));
       }
       console.log(promises);
       const data = await Promise.all(promises);
-      console.log(data);
       data.forEach((item) => {
         result = [...result, item.data];
       });
@@ -27,7 +26,6 @@ function ProductDetail() {
       const elements = ['product', 'styles', 'related', 'reviews', 'meta', 'questions', 'cart'];
       result.forEach((item, index) => {
         final[elements[index]] = item;
-        console.log(final);
       });
       setDetail(final);
     };
