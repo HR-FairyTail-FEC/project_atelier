24eb2f9b9f23563550421a6a9bbcc8d689023131
garofalof <fgarofalo1493@gmail.com>
2022-04-04 16:07:22 -0700

    completed gallery and carousel

--- public/src/Overview.jsx
+++ public/src/Overview.jsx
@@ -1,82 +1,64 @@
 import React, { useState, useEffect } from 'react';
 
 function Overview(props) {
-  let display;
   let feature;
   let photos;
   let details = props.details;
-  let [{featured, active, clicked, start, end, thumbnails}, setState] = useState({
-    featured: 0,
-    active: null,
-    clicked: 0,
-    start: 0,
-    end: 3
-  });
+  const [featured, setFeatured] = useState(0);
+  const [active, setActive] = useState(0);
+  const [thumbnails, setThumbnails] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [display, setDisplay] = useState(<div></div>);
 
-  const handleArrowClick = (photos) => {
-    let last = photos.length - 1;
-    if (end === last) {
-      setState({featured, active, clicked, start: start + 1, end: 0});
-    }
-    if (start === last) {
-      setState({featured, active, clicked, start: 0, end: end + 1});
-    }
-    if (start !== last && end !== last) {
-      setState({featured, active, clicked, start: start + 1, end: end + 1});
+  if (Object.keys(details).length > 0) {
+    if (!loading) {
+      let photos = details.styles.results[0].photos;
+      setLoading(true);
+      setThumbnails(photos);
     }
   }
 
-  if (details.length < 1) {
-    display =
-    <div>
-      <div className="carousel-container"></div>
-      <div className="product-detail-container"></div>
-    </div>
-  } else {
-    photos = details.styles.results[0].photos;
-    let featureUrl = details.styles.results[0].photos[featured].thumbnail_url;
-    feature = <img className="feature-img" src={featureUrl}></img>;
-    let mapped = [];
-    if (end < start) {
-      let sliced = photos.slice(start).concat(photos.slice(0, end + 1));
-      for (let i = 0; i < sliced.length; i++) {
-        let photo = sliced[i];
+  const handleArrowClick = () => {
+    let arr = [...thumbnails];
+    let temp = arr.shift();
+    arr.push(temp);
+    setThumbnails(arr);
+  };
+
+  const handleImageClick = (e) => {
+    setActive(Number(e.target.id));
+    setFeatured(Number(e.target.id));
+  };
+
+  useEffect(() => {
+    if (loading) {
+      let featureUrl = thumbnails[featured].thumbnail_url;
+      let feature = <img className="feature-img" src={featureUrl}></img>;
+      let mapped = [];
+      for (let i = 0; i < 4; i++) {
+        let photo = thumbnails[i].thumbnail_url;
         if (i === active) {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="thumbnail-img active" src={photo.thumbnail_url}></img>)
-        } else if (i === clicked) {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="clicked" src={photo.thumbnail_url}></img>)
+          mapped.push(<img onClick={handleImageClick} key={i} id={i} className="active" src={photo}></img>);
         } else {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="thumbnail-img" src={photo.thumbnail_url}></img>);
+          mapped.push(<img onClick={handleImageClick} key={i} id={i} className="thumbnail-img" src={photo}></img>);
         }
       }
-    }
-    if (start < end) {
-      let sliced = photos.slice(start, end + 1);
-      for (let i = 0; i < sliced.length; i++) {
-        let photo = sliced[i];
-        if (i === active) {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="thumbnail-img active" src={photo.thumbnail_url}></img>)
-        } else if (i === clicked) {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="clicked" src={photo.thumbnail_url}></img>)
-        } else {
-          mapped.push(<img onClick={(e) => setState({featured: e.target.id, active, clicked: e.target.id, start, end})} onMouseEnter={(e) => setState({featured, active: e.target.id, clicked, start, end})} id={i} key={i} className="thumbnail-img" src={photo.thumbnail_url}></img>);
-        }
-      }
-    }
-    display =
-    <div className="left-container">
-      <div className="carousel-container">
-        <div className="thumbnail-container">
-          {mapped}
-          <img onClick={() => handleArrowClick(photos)} className="thumbnail-arrow" src="https://images.squarespace-cdn.com/content/v1/5bbd01503560c334e3d24981/1551746621260-M55SZ54KVQJWN784VA38/arrow-down.png"></img>
+      let carousel =
+      <div className="left-container">
+        <div className="carousel-container">
+          <div className="thumbnail-container">
+            {mapped}
+            <img onClick={handleArrowClick} className="thumbnail-arrow" src="https://images.squarespace-cdn.com/content/v1/5bbd01503560c334e3d24981/1551746621260-M55SZ54KVQJWN784VA38/arrow-down.png"></img>
+          </div>
+          <div className="feature-container">
+            {feature}
+          </div>
         </div>
-        <div className="feature-container">
-          {feature}
-        </div>
+        <div className="product-detail-container"></div>
       </div>
-      <div className="product-detail-container"></div>
-    </div>
-  }
+      setDisplay(carousel);
+    }
+  }, [loading, thumbnails, featured, active]);
 
   return (
     <div className="overview-container">
