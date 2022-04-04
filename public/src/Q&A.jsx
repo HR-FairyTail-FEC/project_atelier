45685573d315a9634932c3628ea7084cf59b62fb
Andy Chan <Chanandy727@gmail.com>
2022-04-04 14:40:38 -0700

    still not showing initial questions on load up

--- public/src/Q&A.jsx
+++ public/src/Q&A.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 
@@ -6,51 +6,121 @@ const QA = (props) => {
   let questions = props.details.questions;
   let display;
   let allQ = [];
-  let allA = [];
-  const sayHi = () => {
-    allA = [];
-    console.log(allA)
-  }
+  let numQShown = 2;
+
+  // let qShown = [];
+  const [qShown, setQShown] = useState([]);
+  // const sayHi = () => {
+  //   if (allQ.length ===1) {
+  //     setQShown(allQ);
+  //   } else if (allQ.length>=2) {
+  //     setQShown([allQ[0], allQ[1]]);
+  //   }
+  //   // setQShown(allQ);
+  //   // console.log('hi');
+  // }
+  // console.log('bye')
 
   if (props.details.length < 1) {
     display = <div>Loading Questions..</div>
   } else {
-    console.log(questions.results)
+    // console.log('always in here')
     questions.results.forEach(result => {
+      console.log(result);
       let allA = [];
       for (let answer in result.answers) {
         allA.push(result.answers[answer]);
       }
-      console.log(result.answers)
-      allQ.push({result: result, answers: allA});
-      console.log(result);
+      allQ.push({ result: result, answers: allA });
     })
+
+    // if (allQ[0] && props.details.length === 1) {
+    //   setQShown([allQ[0]]);
+    // } else if (allQ[1] && allQ[0]) {
+    //   qShown[allQ[0], allQ[1]];
+    // }
     // console.log('this is question', questions.results[0]);
   }
+   const initialize = () => {
+      if (props.details.length === 1 && allQ[0]){
+        const [qShown, setQShown] = useState([allQ[0]]);
+      } else if (props.details.length >= 2 && allQ[1]){
+        const [qShown, setQShown] = useState([allQ[0], allQ[1]]);
+      }
+    }
+    initialize();
+  // useEffect(){
+  //   if (allQ.length === 1) {
+  //     const [qShown, setQShown] = useState([allQ[0]]);
+  //   } else {
+  //     const [qShown, setQShown] = useState([allQ[0], allQ[1]]);
+  //   }
+  // }
 
+  // const initialize = () => {
+
+  // }
+  // initialize();
+
+
+  // if (props.details.length <= 2) {
+  //   setQShown(allQ);
+  // }
+  // let initQ = [];
+  // if (allQ.length <= 2 ) {
+  //   for (let i = 0; i < allQ.length; i++) {
+  //     initQ.push(allQ[i])
+  //   }
+  //   setQShown(initQ);
+  // } else {
+  //   for (let i = 0; i < 2; i++) {
+  //     initQ.push(allQ[i])
+  //   }
+  //   setQShown(initQ);
+  // }
+
+  // if (allQ.length <= 2){
+  //   setQShown(allQ);
+  // }
+
+  const showQ = () => {
+    if (allQ.length <= 2) {
+      setQShown(allQ)
+    }
+    if (allQ.length > 2 && numQShown <= allQ.length) {
+      var newQ = [];
+      for (let i = 0; i < numQShown; i++) {
+        newQ.push(allQ[i]);
+      }
+      setQShown(newQ);
+      numQShown += 2;
+    }
+  }
   return (
     <div>
+      {/* {sayHi()} */}
       <input placeholder='HAVE A QUESTION? SEARCH FOR ANSWERS...'></input>
       {/* <div> */}
-      <div>{allQ.map(q => <div>
+
+      <div>{qShown.map(q => <div>
         <div>
           <h3> Q: {q.result.question_body} </h3>
-          <span>{q.result.asker_name}-{moment(q.result.question_date).format('MMMM Do YYYY')} | Helpful {q.result.question_helpfulness} </span>
+          <span>asked by {q.result.asker_name} on {moment(q.result.question_date).format('MMMM Do YYYY')} | Helpful {q.result.question_helpfulness} </span>
           <div>{q.answers.map(a =>
             <div>
               <h4>A: {a.body}</h4>
               <span>
                 answered on {moment(a.date).format('MMMM Do YYYY')} by {a.answerer_name} | helpful? {a.helpfulness}
-                </span>
-              </div>
-            )}
-        </div>
+              </span><br />
+            </div>
+          )}
+          </div>
         </div>
       </div>
       )}
-      <button onClick={sayHi} >More Answered Questions</button>
-      <button>Add A Question</button>
-    </div>
+        <button onClick={showQ} >More Answered Questions</button>
+        <button>Add A Question</button>
+      </div>
     </div>
 
   );
