ace37a9de7d35632ea94fcdabd63b455716dd75e
Andy Chan <Chanandy727@gmail.com>
2022-04-04 17:51:51 -0700

    Properly shows first two questions and answers unless button clicked

--- public/src/Q&A.jsx
+++ public/src/Q&A.jsx
@@ -6,7 +6,8 @@ const QA = (props) => {
   let questions = props.details.questions;
   let display;
   let allQ = [];
-  let numQShown = 2;
+  const [numQShown, setNumQ] = useState(2);
+  const [numAShown, setNumA] = useState(2);
   let index = 0;
   // let qShown = [];
   // const [qShown, setQShown] = useState([{ result: props.details.questions[0].results, answers: [] }]);
@@ -22,19 +23,19 @@ const QA = (props) => {
   //   // console.log('hi');
   // }
   // console.log('bye')
-  useEffect(() => {
-    console.log('here')
+  // useEffect(() => {
+  //   console.log('here')
 
-    console.log(allQ)
-    if (allQ.length === 1) {
-      // const [qShown, setQShown] = useState([allQ[0]]);
-      setQShown([allQ[0]])
-    } else if (allQ.length >= 2) {
-      // const [qShown, setQShown] = useState([allQ[0], allQ[1]]);
-      setQShown([allQ[0], allQ[1]])
-    }
-    // initialize();
-  }, []);
+  //   console.log(allQ)
+  //   if (allQ.length === 1) {
+  //     // const [qShown, setQShown] = useState([allQ[0]]);
+  //     setQShown([allQ[0]])
+  //   } else if (allQ.length >= 2) {
+  //     // const [qShown, setQShown] = useState([allQ[0], allQ[1]]);
+  //     setQShown([allQ[0], allQ[1]])
+  //   }
+  //   // initialize();
+  // }, []);
 
   if (props.details.length < 1) {
     display = <div>Loading Questions..</div>
@@ -86,19 +87,14 @@ const QA = (props) => {
   //   setQShown(allQ);
   // }
 
-  const showQ = () => {
-    if (allQ.length <= 2) {
-      setQShown(allQ)
-    }
-    if (allQ.length > 2 && numQShown <= allQ.length) {
-      var newQ = [];
-      for (let i = 0; i < numQShown; i++) {
-        newQ.push(allQ[i]);
-      }
-      setQShown(newQ);
-      numQShown += 2;
-    }
+  const showMoreQ = () => {
+    setNumQ(numQShown + 2);
   }
+
+  const showMoreA = () => {
+    setNumA(numAShown + 2);
+  }
+
   return (
     <div>
 
@@ -109,19 +105,21 @@ const QA = (props) => {
         <div>
           <h3> Q: {q.result.question_body} </h3>
           <span>asked by {q.result.asker_name} on {moment(q.result.question_date).format('MMMM Do YYYY')} | Helpful {q.result.question_helpfulness} </span>
-          <div>{q.answers.map(a =>
+          <div>{q.answers.slice(0, numAShown).map(a =>
             <div>
               <h4>A: {a.body}</h4>
               <span>
                 answered on {moment(a.date).format('MMMM Do YYYY')} by {a.answerer_name} | helpful? {a.helpfulness}
               </span><br />
             </div>
+
           )}
           </div>
+          <div> {(q.answers.length > 2 && numAShown <= q.answers.length) && <button onClick = {showMoreA}> More Answers </button>} </div>
         </div>
       </div>
       )}
-        <button onClick={showQ} >More Answered Questions</button>
+        <button onClick={showMoreQ}>More Answered Questions</button>
         <button>Add A Question</button>
       </div>
     </div>
